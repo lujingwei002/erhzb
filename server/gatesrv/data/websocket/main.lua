944d7cb8e42efa1c15f39488d0c38e76b99affbb
ljw <ljw@localhost.localdomain>
2016-04-13 08:35:27 +0800

    修改帧长度，兼容chrome浏览器

--- server/gatesrv/data/websocket/main.lua
+++ server/gatesrv/data/websocket/main.lua
@@ -6,6 +6,7 @@ function dispatch(sid, data, datalen)
     if Gameclient.is_connect() then
         Gameclient.send(sid, data, datalen)
     else
-        Websocket.send_string_frame(sid, "逻辑服务器末连接")
+        --Websocket.send_string_frame(sid, "逻辑服务器末连接")
+        Websocket.send_binary_frame(sid, data, datalen)
     end
 end
